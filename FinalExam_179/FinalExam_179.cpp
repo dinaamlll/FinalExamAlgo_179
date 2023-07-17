c187033c47ce2400fd0b0747d0e00821e8a2bec3
dinaamlll <dinamaliaa17@gmail.com>
2023-07-17 15:18:26 +0700

    done

--- FinalExam_179/FinalExam_179.cpp
+++ FinalExam_179/FinalExam_179.cpp
@@ -1,20 +1,119 @@
-// FinalExam_179.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+class Node {
+public:
+	string nama;
+	int jumlah;
+	string tipe;
+	Node* next;
+	Node* prev;
+};
+class ManajemenProduk {
+private:
+	Node* head;
+public:
+	ManajemenProduk() {
+		head = NULL;
+	}
+	void tambahProduk() {
+		string nama;
+		int jumlah;
+		string tipe;
+		cout << "========== TAMBAH PRODUK ==========" << endl;
+		cout << "Nama Produk : ";
+		getline(cin, nama);
+		cout << "Jumlah Produk : ";
+		cin >> jumlah;
+		cin.ignore(); // Membersihkan buffer input
+		cout << "Tipe Produk : ";
+		getline(cin, tipe);
+		cout << "Tipe Produk : ";
+		cin >> tipe;
+		cin.ignore();
+		cout << "Produk berhasil ditambahkan!" << endl;
+	}
+	void tampilkanSemuaProduk() {
+		cout << "========== SEMUA DATA PRODUK ==========" << endl;
+		if (head == NULL) {
+			cout << "Belum ada data produk yang tersimpan." << endl;
+		}
+		else {
+			Node* current = head;
+			int posisi = 0;
+			while (current != NULL) {
+				posisi++;
+			}
+		}
+	}
+	void cariProdukByNama() {
+		cout << "========== CARI PRODUK BERDASARKAN NAMA ==========" << endl;
+		if (head == NULL) {
+			cout << "Belum ada data produk yang tersimpan." << endl;
+		}
+		else {
+			string targetNama;
+			cout << "Nama Produk yang dicari: ";
+			getline(cin, targetNama);
+			Node* current = head;
+			int posisi = 0;
+			while (current != NULL) {
+				posisi++;
+				string nama;
+				cin >> nama;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+			}
+			if (current == NULL) {
+				cout << "Nama produk " << targetNama << " TIDAK DITEMUKAN" <<
+					endl;
+			}
+		}
+	}
+	void algorithmaSortByJumlahProduk() {
+		if (head == NULL || head->next == NULL) {
+			return; // Tidak perlu sorting jika hanya terdapat 0 atau 1 node
+		}
+		Node* current;
+		Node* last = NULL;
+		current = head;
+		while (current != NULL && current->next) {}
+	}
+};
+int main() {
+	ManajemenProduk manajemenProduk;
+	int pilihan;
+	do {
+		cout << "========== APLIKASI MANAJEMEN DATA PRODUK ==========" << endl;
+		cout << "1. Tambah Produk" << endl;
+		cout << "2. Tampilkan Semua Produk" << endl;
+		cout << "3. Cari Produk berdasarkan Nama" << endl;
+		cout << "4. Urutkan Produk berdasarkan Jumlah" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "Pilihan: ";
+		cin >> pilihan;
+		cin.ignore();
+		switch (pilihan) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			cout << "Terima kasih! Program selesai." << endl;
+			break;
+		default:
+			cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
+		}
+		cout << endl;
+	} while (pilihan != 5);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//2. Double Linked List, Single Linked List
+//3. queue adalah prinsip yang menggunakan FIFO (First In First Out) yang berarti elemen yang pertama kali masuk maka akan pertama kali keluar. element yang pertama dimasukkan yaitu front, dan element akhir disebut rear. operasi queue ada 2, yaitu insert dan delete 
+//4. stack adalah prinsip yang menggunakan Last In First Out (LIFO) yang berarti data yang masuk paling akhir adalah data yang keluar paling awal. operasi stack ada 2 yaitu push dan pop. push yaitu untuk menambahkan data dan pop untuk mengambil data
+//5. a ) memiliki 5 kedalaman
+//5. b ) inorder, yaitu simpul-simpul dalam pohon dengan urutan kiri-akar-kanan
